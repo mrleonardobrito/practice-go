507487ec27cf9e58b31c9734146ae3eb25468eaa
Leonardo Brito <lbritogit@gmail.com>
2022-08-29 09:06:49 -0300

    metade do módulo 1

---
+++ fundamentos/arrays e slices/arrays-e-slices.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var array1 = [5]int{1, 2, 3, 4, 5}
+	var slice = []int{1, 2, 3, 4, 5, 6, 7}
+
+	fmt.Println(array1)
+	fmt.Println(slice[6])
+
+	var slice2 []int = array1[2:4]
+
+	fmt.Println(reflect.TypeOf(array1))
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(slice2)
+
+	// ARRAYS INTERNOS
+
+	slice3 := make([]float32, 10, 15)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+}
